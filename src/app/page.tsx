a6e6e61fb923f36ac9c6170bddd1027c71a2ba03
Wasim <wasim.rashid@developers.studio>
2023-03-07 15:16:12 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
+import Header from "@/components/Header";
 import { Button } from "@chakra-ui/button";
 import { Input } from "@chakra-ui/input";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline text-center">Metaverse</h1>
-      <Input type="text" placeholder="enter value" />
-      <Button>Submit</Button>
+      <Header></Header>
     </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,29 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button, Container, Flex, SimpleGrid } from '@chakra-ui/react'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import logo from '../../public/images/logo.webp'
 
 export default function Header() {
   return (
     <>
-        <Box>
-            <div>This is header</div>
-        </Box>
+        <Container>
+          <SimpleGrid templateColumns={'repeat(3, 1fr)'}>
+            <Box>
+              <Image src={logo} alt='panaverse-logo'></Image>
+            </Box>
+            <Flex>
+              <Link href={'/'}>Home</Link>
+              <Link href={'/'}>Syllabus</Link>
+              <Link href={'/'}>Explore</Link>
+              <Link href={'/'}>About</Link>
+              <Link href={'/'}>Contact</Link>
+            </Flex>
+            <Box>
+              <Button>Apply</Button>
+            </Box>
+          </SimpleGrid>
+        </Container>
     </>
   )
 }
