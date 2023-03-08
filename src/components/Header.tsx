9ea727681d1acb6c9b6b5c135bdf5bae9f9c703b
Wasim <wasim.rashid@developers.studio>
2023-03-08 15:14:14 +0500

    header complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,31 @@
-import { Box, Button, Container, Flex, SimpleGrid } from '@chakra-ui/react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import logo from '../../public/images/logo.webp'
+import { Box, Button, Container, Flex, SimpleGrid } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "../../public/images/logo.webp";
+import { HiMenu } from "react-icons/hi";
 
 export default function Header() {
   return (
-    <>
-        <Container>
-          <SimpleGrid templateColumns={'repeat(3, 1fr)'}>
-            <Box>
-              <Image src={logo} alt='panaverse-logo'></Image>
-            </Box>
-            <Flex>
-              <Link href={'/'}>Home</Link>
-              <Link href={'/'}>Syllabus</Link>
-              <Link href={'/'}>Explore</Link>
-              <Link href={'/'}>About</Link>
-              <Link href={'/'}>Contact</Link>
-            </Flex>
-            <Box>
-              <Button>Apply</Button>
-            </Box>
-          </SimpleGrid>
-        </Container>
-    </>
-  )
+    <div className=" flex justify-between items-center px-[20px] md:px-[60px] py-6 bg-indigo-50 w-full">
+      <div>
+        <Link href={"/"}>
+          <Image src={logo} alt="panaverse-logo"></Image>
+        </Link>
+      </div>
+      <div className=" md:flex items-center space-x-14 text-lg font-semibold hidden">
+        <Link href={"/"} className="hover:text-blue-600">Home</Link>
+        <Link href={"/"} className="hover:text-blue-600">Explore</Link>
+        <Link href={"/"} className="hover:text-blue-600">About</Link>
+        <Link href={"/"} className="hover:text-blue-600">Contact</Link>
+        <div>
+          <button className=" bg-blue-600 px-6 py-2 rounded-lg hover:bg-blue-900 text-white font-bold">
+            Apply
+          </button>
+        </div>
+        
+      </div>
+      <div className=" md:hidden text-3xl items-center"><HiMenu></HiMenu></div>
+    </div>
+  );
 }
