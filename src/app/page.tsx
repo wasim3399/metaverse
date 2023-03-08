e4c610ab88aa1ac1ecbc3b8d6938957d9cd3679a
Wasim <wasim.rashid@developers.studio>
2023-03-08 18:53:28 +0500

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Button } from "@chakra-ui/button";
 import { Input } from "@chakra-ui/input";
@@ -12,13 +13,8 @@ export default function Home() {
   return (
     <main>
       <Header></Header>
-      {/* <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-        <div className="shrink-0"></div>
-        <div>
-          <div className="text-xl font-medium text-black">ChitChat</div>
-          <p className="text-slate-500">You haves a new message!</p>
-        </div>
-      </div> */}
+      <div>main</div>
+      <Footer></Footer>
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "../../public/images/logo.webp";
+
+export default function Footer() {
+  return (
+    <>
+      <div className=" md:flex justify-between px-20 gap-20 bg-black py-10">
+        <div className=" md:w-1/4 md:py-10 py-20 py-5">
+          <h2 className=" text-white font-semibold text-2xl border-l-8 border-blue-600 pl-2">
+            About Us
+          </h2>
+          <div className="pt-10">
+            <Link href={"/"}>
+              <Image src={logo} alt="panaverse-logo"></Image>
+            </Link>
+          </div>
+          <p className=" text-gray-600 text-justify">
+            The Future of the Web is Web 3.0, Metaverse, and Edge Computing.
+            Panaverse DAO is a movement to spread
+          </p>
+        </div>
+        <div className=" md:w-1/4 md:py-10 py-20 py-5">
+          <h2 className=" text-white font-semibold text-2xl border-l-8 border-blue-600 pl-2">
+            Useful Links
+          </h2>
+          <ul className=" pt-10 text-gray-600">
+          <Link href={"/"} className="hover:text-white"><li>Home</li></Link>
+          <Link href={"/"} className="hover:text-white"><li>Syllabus</li></Link>
+          <Link href={"/"} className="hover:text-white"><li>Explore</li></Link>
+          <Link href={"/"} className="hover:text-white"><li>About</li></Link>
+          <Link href={"/"} className="hover:text-white"><li>Contact</li></Link>
+          </ul>
+        </div>
+        <div className=" md:w-1/4 md:py-10 py-20 py-5">
+          <h2 className=" text-white font-semibold text-2xl border-l-8 border-blue-600 pl-2">
+            Follow Us
+          </h2>
+          <ul className=" pt-10 text-gray-600">
+          <Link href={"/"} className="hover:text-white"><li>Facebook</li></Link>
+          <Link href={"/"} className="hover:text-white"><li>LinkedIn</li></Link>
+          <Link href={"/"} className="hover:text-white"><li>Twitter</li></Link>
+          <Link href={"/"} className="hover:text-white"><li>Github</li></Link>
+          <Link href={"/"} className="hover:text-white"><li>Youtube</li></Link>
+          </ul>
+        </div>
+        <div className=" md:w-1/4 md:py-10 py-20 py-5">
+          <h2 className=" text-white font-semibold text-2xl border-l-8 border-blue-600 pl-2">
+            Contact Us
+          </h2>
+          <ul className=" pt-10 text-gray-600">
+            <li>+92 123 4567890</li>
+            <li>wasim.iqtm@gmail.com</li>
+            <li>Lahore, Pakistan</li>
+          </ul>
+        </div>
+      </div>
+      <div className=" md:justify-between md:flex px-20 py-5 bg-gray-100">
+        <div className=" font-bold">All Rights Reserved © | <br className=" md:hidden"></br> Ahmed Raza Shaikh</div>
+        <div>Legals | Terms of use | Privacy Policy</div>
+      </div>
+    </>
+  );
+}
