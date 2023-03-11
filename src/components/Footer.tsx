a360eedfc73a740163e9ae42c3c18b98a7e3f3e9
wasim939 <wasim.iqtm@gmail.com>
2023-03-11 23:05:25 +0500

    fix issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import logo from "../../public/images/logo.webp";
-import { AiOutlineMail, AiOutlinePhone, IconName } from "react-icons/ai";
+import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
 
 export default function Footer() {
   return (
